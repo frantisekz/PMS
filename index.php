cbdd3519b8623935ea5cd1d0fa2cee8bd8920e91
František Zatloukal <Zatloukal.Frantisek@gmail.com>
2014-06-19 23:54:03 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,27 +1,26 @@
 <?php
-include("config.php");
-if (empty($pms_domain))
-	{
-	// This behavior will be changed so
-	// if there is not defined $pms_domain 
-	// the PMS will try to autodetect it
-	die("Check your config.php file!");
-	}
-session_start();
+if(!include("config.php"))
+{
+	die("<h1>Check your config.php!</h1>");
+}
+
 if (isset($_GET['page']))
 	{
 	$page = $_GET['page'] . ".php";
 	$pagename = $_GET['page'];
 	}
+
 else
 	{
-	$page = "home.php";
-	$pagename = "home";
+	$page = "Home.php";
+	$pagename = "Home";
 	}
+
 if ($pms_debug_enabled == 0)
 	{
 	error_reporting(0);
 	}
+
 include("functions.php");
 include ("themes/" . $pms_theme . "/html/header.php");
 include ("themes/" . $pms_theme . "/html/body.php");
