afe687be5197c0ef403d5168560bf274df95157f
František Zatloukal <Zatloukal.Frantisek@gmail.com>
2014-06-20 00:06:10 +0200

    Remove trash

--- pages/home.php
+++
@@ -1,9 +0,0 @@
-<h1>Free and Fast CMS</h1>
-<p class="text-left">
-Why use PMS?<br/>
-*modern "flat" design<br/>
-*fast and clean codebase<br/>
-*fully customisable<br/>
-*completely free and open<br/>
-*only decent version of PHP is required<br/>
-</p>
